ffc94b1263165e6cee7afb94e66ac3483f3ae100
Satish Nayak <satish_nayak@epam.com>
2024-12-23 11:54:20 -0500

    http with react

--- react-with-httpapi-backend/src/components/AvailablePlaces.jsx
+++ react-with-httpapi-backend/src/components/AvailablePlaces.jsx
@@ -10,15 +10,21 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   const[availablePlaces, setAvailablePlaces] = useState([]);
 
-  useEffect(() => {fetch('http://localhost:3000/places')
-    .then((response) => {
-      return response.json();
-    })
-    .then((resData) => {
+  useEffect(() => {
+    async function fetchPlaces(){
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
       setAvailablePlaces(resData.places);
-    });},[]);
+    }
+    fetchPlaces();
 
-  
+  },[]);
+
+  async function fetchPlaces(){
+    const response = await fetch('http://localhost:3000/places');
+    const resData = await response.json();
+    setAvailablePlaces(resData.places);
+  }
 
   return (
     <Places
